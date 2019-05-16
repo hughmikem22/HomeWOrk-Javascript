255aa3779e24a0ab97d2ba91dcec098d9614ea90
hughmikem22 <50379908+hughmikem22@users.noreply.github.com>
2019-05-16 13:00:17 -0500

    Add files via upload

--- StarWars.js
+++ StarWars.js
@@ -1,17 +1,41 @@
-let Hero ='Luke Skywalker';
-let Villain = 'Darth Maul';
+let Jedi = 'Luke Skywalker';
+let Sith = ('Darth Vader', 'Darth Maul');
+
+switch (Jedi) {
+    case 'Luke Skywalker':
+    console.log('You have Chosen wisely Young one');
+    
+
+    console.log(' May the Force be with you');
+        break;
+} 
 
 
 
-if (Hero === 'Darth Maul'){
+if (Sith === 'Darth Maul'){
     console.log('You Have chosen the wrong path Padawan');
 }
-else if(Hero === 'Darth Vader'){
+else if(Jedi === 'Darth Vader'){
     console.log('Luke I am Your Father');
 }
         // only variables can be on the left hand side
-else if('Darth Maul' == Villain){
+else if('Darth Maul' == Sith){
     console.log('You have defied the Dark Side for the last time!!');
     
 }
-console.log(`our hero is ${Hero}`);
+console.log(`our Dark Lord is ${Sith}`);
+
+
+
+
+
+function theForce(){
+    var Master = Math.floor(Math.random() * 3); 
+    var Master1 = Math.floor(Math.random() * 3); 
+    let allegiance = [' Master is Obi Wan',' Master is Qui Gon',' Master is Darth Sidious','Master is Bobba Fett',];
+    var JediName = ['Luke Skywalker', 'Han Solo', 'Mace Windu', 'Anakin Skywalker'];
+    var Padawan = JediName[Master] + allegiance[Master1];
+    console.log(Padawan);
+}
+
+theForce(Jedi);
